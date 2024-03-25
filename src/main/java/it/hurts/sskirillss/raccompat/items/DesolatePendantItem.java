8de098e6b61489aef125251f98f5fbd443f4738a
SSKirillSS <SSTroveSS@Gmail.com>
2024-03-26 02:16:13 +0300

    Added Desolate Pendant model

--- src/main/java/it/hurts/sskirillss/raccompat/items/DesolatePendantItem.java
+++ src/main/java/it/hurts/sskirillss/raccompat/items/DesolatePendantItem.java
@@ -2,11 +2,16 @@ package it.hurts.sskirillss.raccompat.items;
 
 import com.github.alexmodguy.alexscaves.server.entity.ACEntityRegistry;
 import com.github.alexmodguy.alexscaves.server.entity.item.DesolateDaggerEntity;
+import com.google.common.collect.Lists;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import it.hurts.sskirillss.raccompat.init.ItemRegistry;
 import it.hurts.sskirillss.raccompat.misc.RACBackgrounds;
 import it.hurts.sskirillss.raccompat.network.NetworkHandler;
 import it.hurts.sskirillss.raccompat.network.packets.DesolateDaggerRenderStackPacket;
+import it.hurts.sskirillss.relics.client.models.items.CurioModel;
 import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
+import it.hurts.sskirillss.relics.items.relics.base.IRenderableCurio;
 import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.data.cast.CastPredicate;
@@ -22,21 +27,37 @@ import it.hurts.sskirillss.relics.items.relics.base.data.loot.misc.LootCollectio
 import it.hurts.sskirillss.relics.items.relics.base.data.style.StyleData;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Attackable;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.PacketDistributor;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.client.ICurioRenderer;
 
-public class DesolatePendantItem extends RelicItem {
+import java.util.List;
+
+public class DesolatePendantItem extends RelicItem implements IRenderableCurio {
     @Override
     public RelicData constructDefaultRelicData() {
         return RelicData.builder()
@@ -124,6 +145,53 @@ public class DesolatePendantItem extends RelicItem {
 
         addAbilityCooldown(stack, ability, (int) (getAbilityValue(stack, "devastate", "cooldown") * 20));
     }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        CurioModel model = getModel(stack);
+
+        matrixStack.pushPose();
+
+        LivingEntity entity = slotContext.entity();
+
+        model.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTicks);
+        model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+
+        ICurioRenderer.translateIfSneaking(matrixStack, entity);
+        ICurioRenderer.rotateIfSneaking(matrixStack, entity);
+
+        ICurioRenderer.followBodyRotations(entity, model);
+
+        VertexConsumer vertexconsumer = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, RenderType.armorCutoutNoCull(getTexture(stack)), false, stack.hasFoil());
+
+        matrixStack.scale(0.5F, 0.5F, 0.5F);
+
+        model.renderToBuffer(matrixStack, vertexconsumer, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+
+        matrixStack.scale(2F, 2F, 2F);
+
+        matrixStack.popPose();
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public LayerDefinition constructLayerDefinition() {
+        MeshDefinition mesh = HumanoidModel.createMesh(new CubeDeformation(0.4F), 0.0F);
+
+        mesh.getRoot().addOrReplaceChild("body", CubeListBuilder.create().texOffs(45, 36).addBox(-4.0F, 6.25F, -4.7F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(27, 36).addBox(-3.0F, 5.25F, -5.2F, 6.0F, 6.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(16, 35).addBox(-2.0F, 4.25F, -4.7F, 4.0F, 8.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-8.0F, 0.0F, -3.65F, 16.0F, 7.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 0.0F, 1.5F));
+
+        return LayerDefinition.create(mesh, 64, 64);
+    }
+
+    @Override
+    public List<String> headParts() {
+        return Lists.newArrayList("body");
+    }
+
 
     @Mod.EventBusSubscriber
     public static class DesolatePendantEvents {
